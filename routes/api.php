4a3bfecbac963a68dd7cb559259bf7aaf992a067
Vuk Samardžić <samardzic.vuk@gmail.com>
2018-07-30 19:53:35 +0200

    feat: api routes init setup

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::get('todo', 'TodoController@index');
+    Route::post('todo', 'TodoController@create');
+    Route::put('todo/{id}', 'TodoController@update');
+    Route::delete('todo/{id}', 'TodoController@delete');
+});
+
